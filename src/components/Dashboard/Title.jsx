1ab3e8beae72c95cb1ca0b34350f2436b86447fd
Daeyoung Kim <corydykim@gmail.com>
2023-11-25 09:16:59 -0500

    Change title

--- src/components/Dashboard/Title.jsx
+++ src/components/Dashboard/Title.jsx
@@ -1,38 +1,35 @@
-import { Button, Grid, Card, Flex, Heading } from '@aws-amplify/ui-react';
+import { Button, Grid, Card, Flex, Heading } from "@aws-amplify/ui-react";
 
-function Title ({user, signOut}) {
-    return (
-        <Card
-          columnStart="1"
-          columnEnd="-1"
-          rowStart="1"
-          rowEnd="2"
-        >
-          <Grid
-            columnGap="0.5rem"
-            rowGap="0.5rem"
-            templateColumns="3fr 1fr"
-            templateRows="1fr"
-          >
-            <Card>
-              <Heading level='1' fontWeight='bold' fontSize="2rem">Welcome to Eduponics 🌱</Heading>
-            </Card>
-            <Card>
-              <Flex
-                direction="column"
-                justifyContent="left"
-                alignItems="end"
-                alignContent="flex-start"
-                wrap="nowrap"
-                gap="0rem"
-              >
-                <h3>Hello {user.username}!</h3>
-                <Button onClick={signOut}>Sign out</Button>
-              </Flex>
-            </Card>
-        </Grid>
+function Title({ user, signOut }) {
+  return (
+    <Card columnStart="1" columnEnd="-1" rowStart="1" rowEnd="2">
+      <Grid
+        columnGap="0.5rem"
+        rowGap="0.5rem"
+        templateColumns="3fr 1fr"
+        templateRows="1fr"
+      >
+        <Card>
+          <Heading level="1" fontWeight="bold" fontSize="2rem">
+            Welcome to Hydroponics 🌱
+          </Heading>
         </Card>
-    )
+        <Card>
+          <Flex
+            direction="column"
+            justifyContent="left"
+            alignItems="end"
+            alignContent="flex-start"
+            wrap="nowrap"
+            gap="0rem"
+          >
+            <h3>Hello {user.username}!</h3>
+            <Button onClick={signOut}>Sign out</Button>
+          </Flex>
+        </Card>
+      </Grid>
+    </Card>
+  );
 }
 
 export default Title;

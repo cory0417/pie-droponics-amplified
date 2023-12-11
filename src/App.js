885e555a77a9729ada6f9159e209e390c06464ec
Daeyoung Kim <corydykim@gmail.com>
2023-12-11 00:47:24 -0500

    Reconfigure with new components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { Amplify, PubSub } from 'aws-amplify';
-import {GraphQLAPI} from '@aws-amplify/api-graphql';
+import { Amplify } from 'aws-amplify';
 import { AWSIoTProvider } from '@aws-amplify/pubsub';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import { Grid } from '@aws-amplify/ui-react';
 import awsExports from './aws-exports';
-import '@aws-amplify/ui-react/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import TopicSubscriber from './components/Dashboard/Subscriber';
-import Publisher from './components/Dashboard/Publisher';
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import Title from './components/Dashboard/Title';
-import Chart from './components/Dashboard/Chart';
-import { listSensorLogs } from './graphql/queries';
+import CustomizedTabs from "./components/Dashboard/Tabs";
+import { MQTTTab, SensorTab } from './components/Dashboard/TabContent';
+import {subscribe} from './components/Dashboard/Subscriber';
+
+var SUB_TOPIC = "sensor/+";
 
 Amplify.configure(awsExports);
-
-var PUB_TOPIC = 'cmd/';
-var SUB_TOPIC = 'sensor/+/';
-
-// Apply plugin with configuration
 Amplify.addPluggable(
   new AWSIoTProvider({
     aws_pubsub_region: 'us-east-2',
@@ -26,26 +20,37 @@ Amplify.addPluggable(
       'wss://a2medytrnkkh4c-ats.iot.us-east-2.amazonaws.com/mqtt'
   })
 );
-// Querying the database for sensor logs
-const logs = await GraphQLAPI.graphql({ query: listSensorLogs });
-console.log(logs.data.listSensorLogs.items); // need to consider that mqtt will not generate created and updated at fields
 
 function App({ signOut, user }) {
+  const [activeTab, setActiveTab] = useState(0);
+  const [subMsgs, setSubMsgs] = useState([]);
+
+  // Subscription handling logic
+  const handleSubMsg = (payload) => {
+    console.log("Message received", payload);
+    let topic = payload.value[Object.getOwnPropertySymbols(payload.value)[0]];
+    let msg = payload.value.value;
+    setSubMsgs((prevSubMsgs) => [...prevSubMsgs, { topic: topic, msg: msg }]);
+  };
+
+  useEffect(() => {
+    const unsubscribe = subscribe(handleSubMsg, SUB_TOPIC);
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <Grid
-        className="container"
-        columns={{ xs: 4, sm: 8, md: 12 }}
-        gap="0.5rem"
-        rowGap="0.5rem"
-        templateRows="0.5fr 1fr 1fr"
-        templateColumns="1fr 1fr 1fr 1fr"
-      >
-        <Title user={user} signOut={signOut}/>
-        <Publisher topic={PUB_TOPIC}/>
-        <TopicSubscriber topic={SUB_TOPIC} PubSub={PubSub}/>
-      <Chart/>
-      </Grid>
+    <div className="App" >
+      <Title user={user} signOut={signOut}/>
+      <CustomizedTabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === 0 && (
+  <MQTTTab subMsgs={subMsgs} setSubMsgs={setSubMsgs}/>
+)}
+      {activeTab === 1 && (
+        <SensorTab subMsgs={subMsgs}/>
+      )}
     </div>
   );
 }

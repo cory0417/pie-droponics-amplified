27807e846a2e6e2d4f08d081f8dd8a8cc3716de6
Daeyoung Kim <corydykim@gmail.com>
2023-12-11 00:46:37 -0500

    Convert amplify ui to use MUI grid

--- src/components/Dashboard/Title.jsx
+++ src/components/Dashboard/Title.jsx
@@ -1,34 +1,42 @@
-import { Button, Grid, Card, Flex, Heading } from "@aws-amplify/ui-react";
+import { Button } from "@aws-amplify/ui-react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/joy/Typography";
+import Face6Icon from "@mui/icons-material/Face6";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Box } from "@mui/system";
+import Paper from "@mui/material/Paper";
 
 function Title({ user, signOut }) {
   return (
-    <Card columnStart="1" columnEnd="-1" rowStart="1" rowEnd="2">
+    <Paper style={{ backgroundColor: "#EEC759", width: "100%" }}>
       <Grid
-        columnGap="0.5rem"
-        rowGap="0.5rem"
-        templateColumns="3fr 1fr"
-        templateRows="1fr"
+        container
+        spacing={1}
+        alignItems={"center"}
+        justifyContent={"space-between"}
       >
-        <Card>
-          <Heading level="1" fontWeight="bold" fontSize="2rem">
-            Welcome to Hydroponics 🌱
-          </Heading>
-        </Card>
-        <Card>
-          <Flex
-            direction="column"
-            justifyContent="left"
-            alignItems="end"
-            alignContent="flex-start"
-            wrap="nowrap"
-            gap="0rem"
+        <Grid item xs={10}>
+          <Typography
+            level="h1"
+            textAlign={"left"}
+            textColor={"white"}
+            marginInlineStart={10}
           >
-            <h3>Hello {user.username}!</h3>
-            <Button onClick={signOut}>Sign out</Button>
-          </Flex>
-        </Card>
+            Welcome to Hydroponics 🌱
+          </Typography>
+        </Grid>
+        <Grid item xs={2} container alignItems={"center"} marginY={1}>
+          <Box display="flex" flexDirection={"column"}>
+            <Typography fontSize={20} textAlign={"center"}>
+              Hello {user.username}! <Face6Icon />
+            </Typography>
+            <Button onClick={signOut} backgroundColor={"#FFF7D4"}>
+              <LogoutIcon />
+            </Button>
+          </Box>
+        </Grid>
       </Grid>
-    </Card>
+    </Paper>
   );
 }
 
